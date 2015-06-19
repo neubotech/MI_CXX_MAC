a42088a617d56ac85829101846a596a693fb5058
neubotech <neubotech@gmail.com>
2015-06-19 09:58:08 -0700

    Initial Commit

---
+++ MI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MI
+//
+//  Created by Nan Tian on 6/19/15.
+//  Copyright (c) 2015 Nan Tian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
